44421f63dee89e7350e67718aaeeb9b685f8ee95
kokon322 <chornuy322@gmail.com>
2021-01-06 15:42:17 +0100

    finish with MovieController

--- src/main/java/com/lesson11/lesson11/controller/MovieController.java
+++ src/main/java/com/lesson11/lesson11/controller/MovieController.java
@@ -1,6 +1,34 @@
 package com.lesson11.lesson11.controller;
 
+import com.lesson11.lesson11.entity.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class MovieController {
 
+    private List<Movie> movies = new ArrayList<>();
+
+    {
+        movies.add(new Movie(1, "Assassin", 158));
+        movies.add(new Movie(2, "Harry Potter", 142));
+        movies.add(new Movie(3, "Uno", 224));
+    }
+
+    @GetMapping(value = "/movies")
+    public List<Movie> getAllMovies() {
+        return movies;
+    }
+
+    @PostMapping(value = "/movies")
+    public Movie createMovie(@RequestBody Movie movie){
+        movies.add(movie);
+        return movie;
+    }
 
 }
